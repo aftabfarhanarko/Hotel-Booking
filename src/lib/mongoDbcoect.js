462e593b274c7841a68349748ca629f289567e05
Aftab Farhan <aftabfarhan324@gmail.com>
2026-01-13 17:34:51 +0600

    set monngodb collections

---
+++ src/lib/mongoDbcoect.js
@@ -0,0 +1,19 @@
+const uri = `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@clustermyfirstmongodbpr.2cecfoe.mongodb.net/?appName=ClusterMyFirstMongoDbProject`;
+const dbname = process.env.MONGODB_NAME;
+export default collections = {
+  HOTEL: "hoteldata",
+  USER: "users",
+};
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = (cname) => {
+  return client.db(dbname).collection(cname);
+};
